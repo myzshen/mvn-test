4c40574e20acfb0e4b5ee674cfed324a00e1ba03
myzshen <myzshen@gmail.com>
2020-06-28 16:45:17 +0000

    Add trailing Welcome message

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Jenkins World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Jenkins World! Welcome!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Jenkins World!\"");
+            fail("\"message\" is not \"Hello Jenkins World! Welcome!\"");
         }
     }
 
